6119a12b0f01701db49011dbff6c5a7e8d0f0194
JoaoVitor83 <vititor666demon@gmail.com>
2023-01-20 11:43:22 -0300

    Implementação do salvamento de data e atualização da página

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 const form = document.querySelector("form")
 const nlw = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-07", "01-08"],
-  water: [
-    "01-01",
-    "01-02",
-    "01-04",
-    "01-05",
-    "01-06",
-    "01-07",
-    "01-08",
-    "01-09",
-    "01-10",
-    "01-11",
-    "01-12",
-    "01-13",
-  ],
-  food: ["01-02", "01-03", "01-04", "01-05"],
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+
+  const dayExists = nlw.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso ❌")
+    return
+  }
+
+  nlw.addDay(today)
+  alert("Dia adicionado com sucesso ✔")
 }
+
+function save() {
+  localStorage.setItem("jsECMA1003105", JSON.stringify(nlw.data))
+}
+
+const data = JSON.parse(localStorage.getItem("jsECMA1003105")) || {}
 
 nlw.setData(data)
 
